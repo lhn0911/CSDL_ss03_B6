595dc9203c5cb0fe4eb4af8d94ff4dcd50fa347a
lhn0911 <154567669+lhn0911@users.noreply.github.com>
2025-02-05 08:34:18 +0700

    Add files via upload

--- Bai6.sql
+++ Bai6.sql
@@ -1,12 +1,15 @@
-create table HoaDon(
-MaHD int primary key,
-NgayLap date,
-MaSV int,
-foreign key (MaSV) references sinhvien(MaSV)
+create table Books(
+book_id int primary key auto_increment,
+title varchar(255) not null,
+price decimal(10,2) not null,
+stock int not null
 );
-
-insert into HoaDon(MaHD,NgayLap,MaSV)
-values
-(1,'2023-10-01',1),
-(2,'2023-10-02',3);
-select *from HoaDon
+INSERT INTO Books (title, price, stock)
+VALUES
+('To Kill a Mockingbird', 120.00, 10),
+('1984', 90.00, 3),
+('The Great Gatsby', 150.00, 20),
+('Moby Dick', 200.00, 5),
+('Pride and Prejudice', 50.00, 8);
+select * from Books where price> 100;
+delete from Books where title like '%Pride%';
